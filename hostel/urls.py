6a0d09c99c2c96758866f315967cb05fcf780a32
Kushagra-2023 <extraordinarykushagra@email.com>
2025-05-27 22:54:06 +0530

    Additions to settings.py file

--- hostel/urls.py
+++ hostel/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from djangorestframework import routers
 from rest_framework.routers import DefaultRouter
 from .views import StudentViewSet, RoomViewSet, BookingViewSet, ComplaintViewSet
 
