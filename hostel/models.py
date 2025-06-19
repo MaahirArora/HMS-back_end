723c3c863876cf7b9b70fd04de361f131bc54da8
Kushagra-2023 <extraordinarykushagra@email.com>
2025-06-19 22:36:59 +0530

    Created superuser

--- hostel/models.py
+++ hostel/models.py
@@ -1,5 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager
+
+class StudentManager(BaseUserManager):
+    def create_user(self, email, name, phone, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Users must have an email address')
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name, phone=phone, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, name, phone, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, name, phone, password, **extra_fields)
+
 
 class Student(AbstractUser):
     name = models.CharField(max_length=100)
@@ -10,6 +33,9 @@ class Student(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name', 'phone']  # Only include fields required in the form
+
+    objects = StudentManager()  # ✅ Attach custom manager
+
 
     def save(self, *args, **kwargs):
         if not self.username:
